ac2c2ccbe877a24c876c506fce2a9e7daa0b9b22
Tamilvanan <tamilvanan.b@outlook.com>
2025-04-17 09:23:22 +0530

    initial commit

---
+++ References.md
@@ -0,0 +1,3 @@
+## References
+
+https://bg.ibelick.com/
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import logo from "../assets/my-logo.png";
+import { FaCode, FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
+
+const NavBar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img
+            src={logo}
+            className="mx-0"
+            width={150}
+            height={150}
+            alt="logo"
+          />
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/tamilvanan-in"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn profile"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://www.github.com/tamilvanan-b"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub profile"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.discordapp.com/users/1326166844452634755"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Discord profile"
+        >
+          <FaDiscord />
+        </a>
+        <a
+          href="https://leetcode.com/u/tamilvanan-b/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn profile"
+        >
+          <FaCode />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
